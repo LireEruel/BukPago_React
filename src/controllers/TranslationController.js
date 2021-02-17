d89741eafd887c5b7522da96db496eea8a3e6eb0
LireEruel <jsh001505@gmail.com>
2021-02-17 18:00:51 +0900

    번역 페이지 완성

--- src/controllers/TranslationController.js
+++ src/controllers/TranslationController.js
@@ -1,8 +1,14 @@
 import axios from 'axios'
 
-
 export default async function requestTranslate(northText) {
-    return await axios.post(
-        '/api/transrate',{northText : northText}, {withCredentials : true}
-    ).catch(error => {return error}).then(res => {return res.status})
+    var data = null;
+    return await axios.get(
+        '/api/translator',{params : {northText : northText}}, {withCredentials : true}
+    ).catch(error => {return error}).then(res => {
+        if(res.data != null){
+            data = res.data.result
+            return data
+        }
+    })
+    return data;
 }
--- src/stores/TranslationStore.js
+++ src/stores/TranslationStore.js
@@ -17,11 +17,8 @@ class TranslationStore {
 
     @action
     translate(northText) {
-        requestTranslate(northText).then(result => {
-            return result
-        }) 
+        return requestTranslate(northText)
     }
-    
 
 }
 export default TranslationStore = TranslationStore.getInstance()
