d9a9da35509c8c95a8637cb6ce0411548dfe057f
LireEruel <jsh001505@gmail.com>
2021-05-05 01:01:18 +0900

    회원가입 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React,{useContext} from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { observer } from 'mobx-react';
 import HomeLayout from './HomeLayout';
@@ -7,11 +7,10 @@ import { makeStyles } from '@material-ui/styles';
 import TranslationView from './components/TranslationView';
 import { createMuiTheme } from '@material-ui/core';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import SignInView from './components/SignInView';
+import SignUpView from './components/SignUpView';
 import DictionaryView from './components/DictionaryView';
-import TrainView from './components/TrainView'
-import {useCookies} from 'react-cookie'
-
+import TrainView from './components/TrainView';
+import { useCookies } from 'react-cookie';
 
 const useStyle = makeStyles((theme) => ({
     '@global': {
@@ -39,30 +38,42 @@ const theme = createMuiTheme({
     },
 });
 
-
 const App = observer((props) => {
     const classes = useStyle();
-    const [cookies, setCookie,removeCookie] = useCookies(['jwt'])
-    const [hasCookie,setHasCookie] = React.useState(false)
-    React.useEffect(()=>{
-        if(cookies['jwt'] != undefined)
-            setHasCookie(true)
-    })
+    const [cookies, setCookie, removeCookie] = useCookies(['jwt']);
+    const [hasCookie, setHasCookie] = React.useState(false);
+    console.log(hasCookie);
+    React.useEffect(() => {
+        if (cookies['jwt'] != undefined) setHasCookie(true);
+    });
     return (
         <div className={classes.root}>
             <MuiThemeProvider theme={theme}>
-                <Router>
-                    <HomeLayout  cookies={cookies} hasCookie={hasCookie} setHasCookie={setHasCookie} removeCookie={removeCookie} hasCookie={hasCookie}>
+                {hasCookie === false ? (
+                    <Router>
                         <Switch>
-                            <Route exact path="/" component={TranslationView} />
-                            <Route exact path="/dictionary" component={DictionaryView} />
-                            <Route exact path="/train" component={TrainView}/>
+                            <Route path="/signup">
+                                <SignUpView setHasCookie={setHasCookie}></SignUpView>
+                            </Route>
                         </Switch>
-                    </HomeLayout>
-                    <Switch> 
-                        <Route path="/sign-in" component={SignInView} />
-                    </Switch>
-                </Router>
+                    </Router>
+                ) : (
+                    <Router>
+                        <HomeLayout
+                            cookies={cookies}
+                            hasCookie={hasCookie}
+                            setHasCookie={setHasCookie}
+                            removeCookie={removeCookie}
+                            hasCookie={hasCookie}
+                        >
+                            <Switch>
+                                <Route exact path="/" component={TranslationView} />
+                                <Route exact path="/dictionary" component={DictionaryView} />
+                                <Route exact path="/train" component={TrainView} />
+                            </Switch>
+                        </HomeLayout>
+                    </Router>
+                )}
             </MuiThemeProvider>
         </div>
     );
