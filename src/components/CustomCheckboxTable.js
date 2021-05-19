9576706ed9794a6bb3d301328ccdb6f382f75d1e
KimDonghyeok <qoophon@gmail.com>
2021-05-20 00:27:09 +0900

    [update] 테이블 파일삭제 기능 구현 [update]  selected local state FileTraslationStore로 이동 하여 로직 구현

--- src/components/CustomCheckboxTable.js
+++ src/components/CustomCheckboxTable.js
@@ -48,54 +48,44 @@ const useCustomTableStyles = makeStyles({
 
 export default observer(function CustomCheckboxTable(props) {
     const classes = useCustomTableStyles();
-    const [selected, setSelected] = useState([]);
     const { FileTranslationStore } = useStores();
 
-    const handleUploadFile = (e) => {
-        let files = e.target.files
+    const returnFileSize = (size) => {
+        if (size === 0) {
+            return 'n/a'
+        }
 
-        FileTranslationStore.uploadFile(files)
+        var sizes = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'];
+        var e = Math.floor(Math.log(size) / Math.log(1024));
+        return (size / Math.pow(1024, e)).toFixed(2) + " " + sizes[e];
+    }
+
+    const handleUploadFile = (event) => {
+        if (event.target.click) {
+            const files = event.target.files;
+            FileTranslationStore.uploadFile(files)
+        }
+    }
+
+    const handleDeleteFile = (event) => {
+        FileTranslationStore.DeleteFiles();
     }
 
     const handleSelectAllClick = (event) => {
-        const newSelected = []
-
-        if (event.target.checked) {
-            FileTranslationStore.originalFiles.forEach((file) => {
-                newSelected.push(file.name)
-            })
-        }
-
-        setSelected(newSelected);
+        FileTranslationStore.setSelectAll(event);
     }
 
     const handleClick = (event, name) => {
-        const selectedIndex = selected.indexOf(name);
-        let newSelected = [];
-
-        if (selectedIndex === -1) {
-            newSelected = newSelected.concat(selected, name);
-        } else if (selectedIndex === 0) {
-            newSelected = newSelected.concat(selected.slice(1));
-        } else if (selectedIndex === selected.length - 1) {
-            newSelected = newSelected.concat(selected.slice(0, -1));
-        } else if (selectedIndex > 0) {
-            newSelected = newSelected.concat(
-                selected.slice(0, selectedIndex),
-                selected.slice(selectedIndex + 1),
-            );
-        }
-
-        setSelected(newSelected);
+        FileTranslationStore.setSelected(name);
     }
 
     const isSelected = (name) => {
-        return selected.indexOf(name) !== -1;
+        return FileTranslationStore.selected.indexOf(name) !== -1;
     }
 
     return (
         <div className={classes.root}>
-            <Toolbar className={clsx(classes.toolbarRoot, { [classes.highlight]: selected.length > 0 })}>
+            <Toolbar className={clsx(classes.toolbarRoot, { [classes.highlight]: FileTranslationStore.selected.length > 0 })}>
                 {
                     FileTranslationStore.numSelected > 0 ? (
                         <Typography className={classes.toolbarTitle} variant="subtitle1" component="div">
@@ -107,13 +97,14 @@ export default observer(function CustomCheckboxTable(props) {
                         </Typography>
                     )
                 }
-                {selected.length > 0 ? (
+                {FileTranslationStore.selected.length > 0 ? (
                     <Button
                         className={classes.button}
                         variant="contained"
                         component="label"
                         color="secondary"
                         endIcon={<DeleteIcon />}
+                        onClick={handleDeleteFile}
                     >
                         삭제
                     </Button>
@@ -122,6 +113,7 @@ export default observer(function CustomCheckboxTable(props) {
                         className={clsx(classes.button, classes.uploadButton)}
                         variant="contained"
                         component="label"
+                        for="upload"
                         endIcon={<PublishIcon />}
                     >
                         업로드
@@ -143,8 +135,8 @@ export default observer(function CustomCheckboxTable(props) {
                         <TableRow>
                             <TableCell padding="checkbox">
                                 <Checkbox
-                                    indeterminate={selected.length > 0 && selected.length < FileTranslationStore.fileCount}
-                                    checked={FileTranslationStore.fileCount > 0 && selected.length === FileTranslationStore.fileCount}
+                                    indeterminate={FileTranslationStore.selected.length > 0 && FileTranslationStore.selected.length < FileTranslationStore.fileCount}
+                                    checked={FileTranslationStore.fileCount > 0 && FileTranslationStore.selected.length === FileTranslationStore.fileCount}
                                     onChange={handleSelectAllClick}
                                 />
                             </TableCell>
@@ -170,9 +162,9 @@ export default observer(function CustomCheckboxTable(props) {
                                             checked={isItemSelected}
                                         />
                                     </TableCell>
-                                    <TableCell className={classes.idCell}>{index}</TableCell>
+                                    <TableCell className={classes.idCell}>{index + 1}</TableCell>
                                     <TableCell className={classes.nameCell}>{fileInfo.name}</TableCell>
-                                    <TableCell className={classes.sizeCell}>{fileInfo.size}</TableCell>
+                                    <TableCell className={classes.sizeCell}>{returnFileSize(fileInfo.size)}</TableCell>
                                 </TableRow>
                             )
                         })}
--- src/stores/FileTranslationStore.js
+++ src/stores/FileTranslationStore.js
@@ -3,21 +3,25 @@ export class FileTranslationStore {
     originalFiles = [];
     translatedFiles = [];
     fileCount = 0;
+    selected = [];
 
     constructor() {
         makeAutoObservable(this, {
             originalFiles: observable,
             translatedFiles: observable,
             fileCount: observable,
-            uploadFile: action
+            selected: observable,
+            uploadFile: action,
+            DeleteFiles: action,
+            setSelected: action,
+            setSelectAll: action,
         });
     }
 
     uploadFile(fileList) {
-        console.log(fileList)
-
         for (const file of fileList) {
             const reader = new FileReader();
+            reader.readAsText(file);
 
             reader.onload = (e) => {
                 const content = e.target.result;
@@ -28,7 +32,7 @@ export class FileTranslationStore {
                     content: content,
                 }
 
-                const index = this.isExist(file);
+                const index = this.findIndex(file);
 
                 if (index === -1) {
                     this.originalFiles.push(fileInfo)
@@ -36,15 +40,53 @@ export class FileTranslationStore {
                 } else {
                     this.originalFiles[index] = fileInfo;
                 }
-
             }
-            reader.readAsText(file);
         }
     }
 
-    isExist(file) {
+    findIndex(file) {
         const fileName = file.name;
 
         return this.originalFiles.findIndex(e => e.name === fileName);
+    }
+
+    DeleteFiles() {
+        for (const name of this.selected) {
+            this.originalFiles = this.originalFiles.filter((file) => file.name !== name)
+        }
+        this.fileCount = this.originalFiles.length;
+        this.selected = [];
+    }
+
+    setSelected(name) {
+        const selectedIndex = this.selected.indexOf(name);
+        let newSelected = [];
+
+        if (selectedIndex === -1) {
+            newSelected = newSelected.concat(this.selected, name);
+        } else if (selectedIndex === 0) {
+            newSelected = newSelected.concat(this.selected.slice(1));
+        } else if (selectedIndex === this.selected.length - 1) {
+            newSelected = newSelected.concat(this.selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+            newSelected = newSelected.concat(
+                this.selected.slice(0, selectedIndex),
+                this.selected.slice(selectedIndex + 1),
+            );
+        }
+
+        this.selected = [...newSelected]
+    }
+
+    setSelectAll(event) {
+        const newSelected = []
+
+        if (event.target.checked) {
+            this.originalFiles.forEach((file) => {
+                newSelected.push(file.name)
+            })
+        }
+
+        this.selected = [...newSelected]
     }
 };
