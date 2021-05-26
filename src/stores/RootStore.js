774f543ed933933d9f16d1244f85e06737a9fd05
KimDonghyeok <qoophon@gmail.com>
2021-05-27 08:53:34 +0900

    [update] TranslationApiStore mobx 의존성 추가

--- src/stores/RootStore.js
+++ src/stores/RootStore.js
@@ -1,9 +1,12 @@
 import { FileTranslationStore } from './FileTranslationStore';
+import { TranslationApiStore } from './TranslationApiStore';
 
 export class RootStore {
     FileTranslationStore;
+    TranslationApiStore;
 
     constructor() {
         this.FileTranslationStore = new FileTranslationStore();
+        this.TranslationApiStore = new TranslationApiStore();
     }
 }
