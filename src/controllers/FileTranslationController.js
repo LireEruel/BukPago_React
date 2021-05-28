c6a62cad3b2caa9764c8ad2b0008d18e9dc2200e
KimDonghyeok <qoophon@gmail.com>
2021-05-28 16:49:13 +0900

    [update] axios.get axios.post로 바꿈

--- src/controllers/FileTranslationController.js
+++ src/controllers/FileTranslationController.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default async function requestFileTranslate(_FILES) {
-    return await axios.get('/api/file-translator',
+    return await axios.post('/api/file-translator',
         {
             paramsSerializer: () => { return JSON.stringify(_FILES); },
             withCredentials: true
